3628b5fa43f6b60cb7030408b3c4ec750d2f396c
negrete79 <116903139+negrete79@users.noreply.github.com>
2026-07-29 00:07:41 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,44 +1,9 @@
-var CACHE = 'incubapro-v1';
-
-self.addEventListener("install", function(e) {
-  e.waitUntil(
-    caches.open(CACHE).then(function(c) {
-      return c.addAll([
-        './',
-        './index.html',
-        './style.css',
-        './app.js',
-        './manifest.json'
-      ]);
-    })
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", function(e) {
-  e.waitUntil(
-    caches.keys().then(function(keys) {
-      return Promise.all(
-        keys.filter(function(k) { return k !== CACHE; })
-            .map(function(k) { return caches.delete(k); })
-      );
-    })
-  );
-  e.waitUntil(clients.claim());
-});
-
-self.addEventListener("fetch", function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(r) {
-      return r || fetch(e.request).then(function(res) {
-        if (res.status === 200 && res.type === 'basic') {
-          var cl = res.clone();
-          caches.open(CACHE).then(function(c) { c.put(e.request, cl); });
-        }
-        return res;
-      });
-    }).catch(function() {
-      return new Response('Offline', { status: 503 });
-    })
-  );
-});
+return c.addAll([
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+]);
